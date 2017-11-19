932fa58663e512234c6bb5897ed5efbfe12245da
kinshines <1176807665@qq.com>
2017-11-19 20:16:25 +0800

    runtime permission for qrcode

--- app/src/main/java/com/inktech/autoseal/ui/QRCodeReaderActivity.java
+++ app/src/main/java/com/inktech/autoseal/ui/QRCodeReaderActivity.java
@@ -1,8 +1,13 @@
 package com.inktech.autoseal.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PointF;
 import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -32,7 +37,21 @@ public class QRCodeReaderActivity extends AppCompatActivity implements QRCodeVie
     @Override
     protected void onStart() {
         super.onStart();
-        mQRCodeView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            mQRCodeView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            TakePhotoActivity.ConfirmationDialogFragment
+                    .newInstance(R.string.camera_permission_confirmation,
+                            new String[]{Manifest.permission.CAMERA},
+                            Constants.REQUEST_CAMERA_PERMISSION,
+                            R.string.camera_permission_not_granted)
+                    .show(getSupportFragmentManager(), Constants.FRAGMENT_DIALOG);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    Constants.REQUEST_CAMERA_PERMISSION);
+        }
 
         mQRCodeView.startSpotAndShowRect();
     }
@@ -71,5 +90,22 @@ public class QRCodeReaderActivity extends AppCompatActivity implements QRCodeVie
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constants.REQUEST_CAMERA_PERMISSION:
+                if (permissions.length != 1 || grantResults.length != 1) {
+                    throw new RuntimeException("Error on requesting camera permission.");
+                }
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, R.string.camera_permission_not_granted,
+                            Toast.LENGTH_SHORT).show();
+                }
+                // No need to start camera here; it is handled by onResume
+                break;
+        }
     }
 }
